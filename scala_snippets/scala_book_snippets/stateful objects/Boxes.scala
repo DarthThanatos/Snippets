d1464f37e9847b9ab3c16d9036eb39d73315288f
DarthThanatos <robert.bielas@interia.pl>
2017-09-25 17:09:38 +0200

    stateful objects & logic gates simulation framework

---
+++ scala_snippets/scala_book_snippets/stateful objects/Boxes.scala
@@ -0,0 +1,24 @@
+import simulation._
+
+object MySimulation extends CircuitSimulation{
+	def InverterDelay = 1 
+	def AndGateDelay = 3 
+	def OrGateDelay = 5
+}
+
+import MySimulation._
+
+val input1, input2, sum, carry = new Wire 
+
+probe("sum", sum)
+probe("carry", carry)
+
+halfAdder(input1, input2, sum, carry) 
+input1 setSignal true
+
+run()
+
+input2 setSignal true
+
+run()
+//79UX-BM33-QUWT-ZRW2
---
+++ scala_snippets/scala_book_snippets/stateful objects/run.bat
@@ -0,0 +1,2 @@
+call fsc -cp bin -d bin BasicCircuitSimulation.scala CircuitSimulation.scala Simulation.scala
+call scala -cp bin Boxes.scala
