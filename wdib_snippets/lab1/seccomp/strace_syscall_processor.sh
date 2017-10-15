b851b717957a99fb88d7b2ca655c314d0572ff2c
DarthThanatos <robert.bielas@interia.pl>
2017-10-15 09:29:44 +0200

    redirections

--- wdib_snippets/lab1/seccomp/strace_syscall_processor.sh
+++ wdib_snippets/lab1/seccomp/strace_syscall_processor.sh
@@ -1,1 +1,4 @@
-strace -c -S name -f "${@:2}" 2>&1 1>/dev/null | tail -n +3 | head -n -2 | awk '{printf "ADD(%s);\n",$(NF)}'> $1
+strace -c -S name -f "${@:2}" 2>&1 1>/dev/null \
+	| tail -n +3 \
+	| head -n -2 \
+	| awk '{printf "ADD(%s);\n",$(NF)}'> $1
--- wdib_snippets/lab1/seccomp/strace_syscalls.sh
+++ wdib_snippets/lab1/seccomp/strace_syscalls.sh
@@ -1,3 +1,6 @@
-strace -c -S name -f $@ 2>&1 1>/dev/null | tail -n +3 | head -n -2 > strace_columns
+strace -c -S name -f $@ 2>&1 1>/dev/null \
+	| tail -n +3 \
+	| head -n -2 > strace_columns
+
 python add_processor.py out
 
