cac0c3937b959193a2d862a13f71fec155e95c5d
DarthThanatos <robert.bielas@interia.pl>
2017-11-16 14:36:58 +0100

    remoting, db loaded

--- rx_scala_snippets/shop_lab5/src/main/scala/shop/Main.scala
+++ rx_scala_snippets/shop_lab5/src/main/scala/shop/Main.scala
@@ -129,12 +129,27 @@ object Main {
     db.printDB()
   }
 
+  def testScoreSorting(): Unit ={
+    val item = Item(new URI("a"), "a", 0, 0)
+    val refItem = Item(new URI("b"), "b", 1, 1)
+    val res1 = List((10, item), (5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item),(5, item))
+    println(appendScoreToSorted(res1, refItem, 7))
+  }
+
   def testCustomer(): Unit ={
     system.actorOf(Props[Customer], "customer")
   }
 
+
+  def appendScoreToSorted(target: List[(Int, Item)], item: Item, itemScore: Int): List[(Int, Item)] ={
+    val biggerVals: List[(Int,Item)] = target.filter( _._1 >= itemScore)
+    val smallerVals = target.filter( _._1 < itemScore)
+    (biggerVals ++ ((itemScore, item) :: smallerVals)).take(10)
+  }
+
   def main(args: Array[String]): Unit ={
     testDB()
+
   }
 }
 
